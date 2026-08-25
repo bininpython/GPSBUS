7fad4717a1f1706952e2d08f4b8261e4ea4dfff5
bininpython <142698210+bininpython@users.noreply.github.com>
2026-08-25 04:32:11 -0300

    fix: use compatible MapLibre namespace import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React,{useEffect,useRef,useState}from 'react'
 import {createRoot} from 'react-dom/client'
-import maplibregl from 'maplibre-gl'
+import * as maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import {supabase,type BusLocation} from './lib/supabase'
 import './styles.css'
